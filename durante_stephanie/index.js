1054d183e37db37edc981d6925aa053239c550bc
durante-stephanie <durantestephanie07@gmail.com>
2025-03-19 23:22:06 +0800

    update js file (#12)
    * update js file

--- durante_stephanie/index.js
+++ durante_stephanie/index.js
@@ -1,7 +1,7 @@
 function validateComment() {
-  let comment_name = document.getElementById("name");
-  let comment_message = document.getElementById("comment");
-  let comment_button = document.getElementById("comment_button");
+  let commentName = document.getElementById("comment_name");
+  let commentMessage = document.getElementById("comment_form");
+  let commentButton = document.getElementById("comment_button");
 
   if (comment_name.value.length < 1 || comment_message.value.length < 1) {
     comment_button.disabled = true;
@@ -10,5 +10,9 @@ function validateComment() {
   }
 }
 
-document.getElementById("name").addEventListener("input", validateComment);
-document.getElementById("comment").addEventListener("input", validateComment);
+document
+  .getElementById("comment_name")
+  .addEventListener("input", validateComment);
+document
+  .getElementById("comment_form")
+  .addEventListener("input", validateComment);
